181096926057ae14b5a037ca8d4c396b41b5384b
EduardoCarvalho262 <eduardoc313@gmail.com>
2024-01-05 21:05:21 -0300

    test: change create product and update product tests

--- src/test/java/com/gft/product/commands/ProductCommandsTests.java
+++ src/test/java/com/gft/product/commands/ProductCommandsTests.java
@@ -8,6 +8,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URL;
+
 
 @SpringBootTest
 public class ProductCommandsTests {
@@ -18,14 +20,17 @@ public class ProductCommandsTests {
         CreateProductCommand createProductCommand = new CreateProductCommand();
         String expectedName = "Teste";
         Double expectedValue = 9.99;
+        String expectedImage =  String.valueOf(ClassLoader.getSystemResource("assets/imageTest.jpg"));
 
         //Act
         createProductCommand.setName("Teste");
         createProductCommand.setValue(9.99);
+        createProductCommand.setImage(String.valueOf(ClassLoader.getSystemResource("assets/imageTest.jpg"));
 
         //Assert
         Assert.assertEquals(expectedName, createProductCommand.getName());
         Assert.assertEquals(expectedValue, createProductCommand.getValue());
+        Assert.assertEquals(expectedImage, createProductCommand.getImage());
     }
 
     @Test
@@ -46,7 +51,8 @@ public class ProductCommandsTests {
     @Test
     public void GiveUpdateACommand_WhenUpdate_ThenReturnANewProduct(){
         //Arrange
-        Product oldProduct = new Product(1,"Teste1", 9.99, (byte) 5);
+        String oldImage = String.valueOf(ClassLoader.getSystemResource("assets/imageTest.jpg"));;
+        Product oldProduct = new Product(1,"Teste1", 9.99, (byte) 5, image);
         UpdateProductCommand updateProductCommand = new UpdateProductCommand();
 
 
@@ -54,10 +60,12 @@ public class ProductCommandsTests {
         updateProductCommand.setId(oldProduct.getId());
         updateProductCommand.setName(oldProduct.getName());
         updateProductCommand.setValue(oldProduct.getValue());
+        updateProductCommand.setValue(oldProduct.getImage());
 
         //Assert
         Assert.assertEquals(oldProduct.getId(), updateProductCommand.getId());
         Assert.assertEquals(oldProduct.getName(), updateProductCommand.getName());
         Assert.assertEquals(oldProduct.getValue(), updateProductCommand.getValue());
+        Assert.assertEquals(oldProduct.getImage(), updateProductCommand.getImage());
     }
 }
